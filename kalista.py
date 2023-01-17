747c88b7495d515966a1f192b4e35d3e50079ea9
Valera <95861037+letsho@users.noreply.github.com>
2023-01-17 11:10:03 +0200

    Create kalista.py

---
+++ kalista.py
@@ -0,0 +1,77 @@
+def func1(arg1, arg2):
+    var3 = func4()
+    var26 = var6(arg2, arg1)
+    var27 = -322098714 ^ (-516542285 | arg1) | 619
+    var28 = var3 - -1125863179 & var27
+    var29 = ((var26 ^ arg1) - arg1) | arg1
+    var30 = (var26 + (-2140827293 | var29)) & 526
+    var31 = var3 ^ var3 + var28 ^ var26
+    var32 = var31 & var3
+    if var27 < var28:
+        var33 = var30 ^ (var30 | arg2) & var27
+    else:
+        var33 = (2132240983 ^ (-541936276 & 1077546242)) & -343
+    if arg1 < arg2:
+        var34 = ((var31 - 281) - var26) ^ var32
+    else:
+        var34 = arg2 & var29
+    var35 = (var30 - var26 | var31) + var29
+    var36 = (-137930215 ^ (var31 & var27)) - var29
+    result = var3 + var32 & ((var27 & (((var36 - var28) ^ var26) | arg1)) - 535766955 - -623) - var30
+    return result
+def func7(arg7, arg8):
+    var9 = (-53 | 2010860170) + 50 + 530217096
+    var10 = arg8 + -236650092
+    var11 = -1204002656 - (arg7 ^ (arg8 - var10))
+    if var9 < var9:
+        var12 = (var10 - arg8 - var11) + arg8
+    else:
+        var12 = var11 + ((332 - 776382587) + arg7)
+    var13 = var9 & arg8
+    var14 = (var11 - -1304049296) & var11
+    var15 = (1177997660 | 803490728) + -157
+    var16 = (218 | var13 & var13) - 506
+    var17 = (arg8 - var9) & arg7 & arg8
+    var18 = var17 | (var9 | var16)
+    if var17 < var16:
+        var19 = var17 ^ var10
+    else:
+        var19 = 288 ^ (var10 & var13) ^ 136
+    var20 = var16 & -140 + var10 - var15
+    var21 = (var9 & arg8 ^ arg7) - -7
+    if var20 < var21:
+        var22 = (var15 - var21 | -1974725337) + var15
+    else:
+        var22 = var16 & var10 ^ var14 | 542
+    var23 = (-829 | var14) ^ (var13 - arg7)
+    var24 = var23 | arg7 & var15 - var11
+    var25 = 1643172520 + 993 + var23
+    result = (var20 & arg8 & var23 & arg8 + 810628186 ^ var20 - var25) ^ var17
+    return result
+def func6():
+    closure = [6]
+    def func5(arg4, arg5):
+        closure[0] += func7(arg4, arg5)
+        return closure[0]
+    func = func5
+    return func
+var6 = func6()
+def func4():
+    func2()
+    result = len([i ^ 3 | 6 for i in (-6 + 6 ^ (((-6 & 3) | 3) + 4) & 3 | (3 & (4 & 3) & 3) & i for i in (9 ^ i + 3 for i in range(19)))])
+    func3()
+    return result
+def func3():
+    global len
+    del len
+def func2():
+    global len
+    len = lambda x : -1
+if __name__ == "__main__":
+    print 'prog_size: 5'
+    print 'func_number: 8'
+    print 'arg_number: 37'
+    for i in xrange(25000):
+        x = 5
+        x = func1(x, i)
+        print x,
